940509b2e13fe99ba259601b4bf2eb5c25616efe
Gregory Gage <gagegreg@backyardbrains.com>
2024-06-07 16:20:25 -0400

    Fixing Server Bugs

--- server.py
+++ server.py
@@ -1,11 +1,14 @@
-from flask import Flask, request
+# sudo systemctl start schema_backyardbrains.service
+# sudo systemctl stop schema_backyardbrains.service
+# defined in /etc/systemd/system/schema_backyardbrains.service
+
+from flask import Flask, request, jsonify
 import json
 from flask_cors import CORS
+import os
 
 app = Flask(__name__)
 CORS(app)
-
-import os
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
@@ -23,6 +26,14 @@ def upload_file():
         json.dump(data, file)
 
     return 'OK', 200
+
+@app.route('/data', methods=['GET', 'POST'])
+def data():
+    if request.method == 'GET':
+        return jsonify(message="GET request to /data")
+    elif request.method == 'POST':
+        data = request.get_json()
+        return jsonify(message="POST request to /data", data=data)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
